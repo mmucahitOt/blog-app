ec13a716cd5d9248587bb9579a5753810b5d98f5
mmucahitOt <m.mucahit0@gmail.com>
2025-08-03 17:12:57 +0300

    default port set to 3000

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
 const NODE_ENV = process.env.NODE_ENV;
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 const MONGODB_URI =
   process.env.NODE_ENV === "test"
     ? process.env.TEST_MONGODB_URI
