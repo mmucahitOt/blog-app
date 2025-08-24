05f6d1d9c7cc9a53761c279e492b6081299ff011
mmucahitOt <m.mucahit0@gmail.com>
2025-08-24 11:41:13 +0300

    chore: update Playwright configuration with new base URL and adjust server timeout settings for E2E tests

--- playwright.config.js
+++ playwright.config.js
@@ -28,7 +28,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: "http://localhost:3000",
+    baseURL: "http://localhost:5173",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
@@ -77,15 +77,15 @@ export default defineConfig({
   webServer: [
     {
       command: "npm run dev:backend:e2e",
-      url: "http://localhost:3000",
+      url: "http://localhost:3000/health",
       reuseExistingServer: !process.env.CI,
-      timeout: 120 * 1000,
+      timeout: 300 * 1000,
     },
     {
       command: "npm run dev:frontend",
       url: "http://localhost:5173",
       reuseExistingServer: !process.env.CI,
-      timeout: 120 * 1000,
+      timeout: 300 * 1000,
     },
   ],
 });
